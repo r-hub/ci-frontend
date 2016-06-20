e7b8cefae817ac6e1500aa135a365451aae4d74a
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-20 14:14:10 +0100

    Prefix API URLs with /-/
    The regular URLs will be for users and repos.

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/webhook', webhook);
-app.use('/check', dokkucheck);
+app.use('/-/webhook', webhook);
+app.use('/-/check', dokkucheck);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
