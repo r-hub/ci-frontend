5c4f265e56ca96d2c84522c883e743e41a0035de
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-28 11:50:51 -0700

    Do not 404 for ping events, confusing when hook is added

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,7 +6,11 @@ router.post('/', function(req, res, next) {
 
     // Drop non-push events
     var event = req.get('X-GitHub-Event');
-    if (event !== 'push') { return next(); }
+    if (event !== 'push') {
+	res.set(200)
+	    .send('{ "status": "OK" }');
+	return;
+    }
 
     queue_job(req.body)
     res.set('Content-Type', 'application/json')
